5d03c1f34dbf9f513c3ac0b2e6544453b6258b96
MBFG9000 <dobridrug111@gmail.com>
2026-05-31 12:55:43 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+  <>
+  <div>
+    <p>Hello</p>
+  </div>
+  <Text/>
+  <Text/>
+  <Text/>
+  </>)
+}
+
+function Text() {
+  return (<div>
+            <p>Hello World</p>
+          </div>)
+}
+
+export default App
